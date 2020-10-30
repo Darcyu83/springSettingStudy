db726367a21116c2f02af6a5ff37e387e1fd1f11
DSY <DSY@DESKTOP-DDP0KCO>
2020-10-30 17:59:22 +0900

    Spring url / url resolver dispatcher setting.xml

---
+++ src/main/java/com/newlecture/web/controller/notice/ListController.java
@@ -0,0 +1,24 @@
+package com.newlecture.web.controller.notice;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class ListController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	System.out.println("index controller");
+		
+		ModelAndView mv = new ModelAndView("notice/list");
+		//mv.addObject("data", "Hello Spring MVC ~ ");
+		//mv.setViewName("/WEB-INF/view/notice/index.jsp");
+		//prefix : /WEB-INF/view/
+		//suffix : .jsp
+		return mv;
+
+	}
+
+}
